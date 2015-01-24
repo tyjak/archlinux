5dfb39fb7b45af9ee1fcf6cfdf6d2bc2ad6a50f7
David Foucher <dev@tyjak.net>
2015-01-24 15:55:36 +0100

    conf pour vimb

--- bin/hotplug_monitor.sh
+++ bin/hotplug_monitor.sh
@@ -2,17 +2,19 @@
 
 export DISPLAY=:0
 export XAUTHORITY=/home/david/.Xauthority
+#OUTPUTSCREEN=HDMI1
+OUTPUTSCREEN=DP2
 
 function connect(){
     #xrandr --output HDMI1 --right-of eDP1 --preferred --primary --output eDP1 --preferred 
-    xrandr --output HDMI1 --auto --output eDP1 --off 
+    xrandr --output $OUTPUTSCREEN --auto --output eDP1 --off 
     [[ -f /usr/bin/feh ]] && sh ~/.fehbg &
 }
   
 function disconnect(){
     #xrandr --output HDMI1 --off 
-    xrandr --output HDMI1 --off --output eDP1 --auto
+    xrandr --output $OUTPUTSCREEN --off --output eDP1 --auto
     [[ -f /usr/bin/feh ]] && sh ~/.fehbg &
 }
      
-xrandr | grep "HDMI1 connected" &> /dev/null && connect || disconnect
+xrandr | grep "$OUTPUTSCREEN connected" &> /dev/null && connect || disconnect
